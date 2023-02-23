657c891f09ad82112bb41d65529d91d75049a05f
ZERONE <zerone012@gmail.com>
2023-02-23 09:13:29 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/study_flutter_04_getx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.study_flutter_04_getx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('라우트 관리 홈'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [],
+        ),
+      ),
+    );
+  }
+}
