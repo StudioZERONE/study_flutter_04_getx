414d3a3b86192016e76109310db2331ccbf445c6
ZERONE <zerone012@gmail.com>
2023-03-07 20:46:06 +0900

    페이지 설명에 타이틀 추가

--- lib/src/pages/dependency/get_put.dart
+++ lib/src/pages/dependency/get_put.dart
@@ -6,14 +6,25 @@ class GetPut extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("Get.put"),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: const [
             Text("DependencyManagePage에서 Get.put을 사용하게 되면 들어오는 페이지"),
+            SizedBox(
+              height: 30,
+            ),
             Text("이 페이지에 들어오면,"),
+            SizedBox(
+              height: 5,
+            ),
             Text("즉시 GetxController를 상속받은 DependencyController를 생성하고,"),
+            SizedBox(
+              height: 5,
+            ),
             Text("페이지에서 나가면 DependencyController를 삭제하게 됨"),
           ],
         ),
